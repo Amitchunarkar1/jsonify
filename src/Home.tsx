ef54dd4d93524e1eaff6cb31933ed5a9ba51fd9a
amitchunarkar1 <amitchunarkar123@hotmail.com>
2024-02-22 23:43:17 +0530

    update changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,40 @@
 import { FunctionalComponent as FC } from "preact";
+import { Box, Container, Typography, TextareaAutosize } from "@mui/material";
 import { useState, useRef } from "preact/hooks";
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
-import ReactJson from 'react-json-view';
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import json from "react-syntax-highlighter/dist/esm/languages/hljs/json";
+import ReactJson from "react-json-view";
 import Button from "./components/button/button";
-import './app.css'
-SyntaxHighlighter.registerLanguage('json', json);
+import "./app.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+SyntaxHighlighter.registerLanguage("json", json);
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#90caf9", // Customize primary color
+    },
+    secondary: {
+      main: "#f48fb1", // Customize secondary color
+    },
+    background: {
+      default: "#121212", // Customize default background color
+      paper: "#212121", // Customize paper background color
+    },
+    text: {
+      primary: "#ffffff", // Customize primary text color
+      secondary: "#bdbdbd", // Customize secondary text color
+    },
+    // You can add more customization options as needed
+  },
+});
 
 // interface HTMLInputElementEvent extends Event {
 //   target: HTMLInputElement & EventTarget;
 // }
 
-const CollapsibleJSONViewer: FC <{ jsonData: string }> = ({ jsonData }) => {
+const CollapsibleJSONViewer: FC<{ jsonData: string }> = ({ jsonData }) => {
   const [collapsed, setCollapsed] = useState<boolean>(false);
 
   const [fullScreen, setFullScreen] = useState<boolean>(false);
@@ -24,19 +47,21 @@ const CollapsibleJSONViewer: FC <{ jsonData: string }> = ({ jsonData }) => {
     setFullScreen(!fullScreen);
   };
 
-
   return (
     <div>
-     <div className="button-group">
+      <div className="button-group">
         <button onClick={toggleCollapsed} className="mb-1">
-          {collapsed ? 'Expand' : 'Collapse'}
+          {collapsed ? "Expand" : "Collapse"}
         </button>
-        <button onClick={toggleFullScreen} className={`mb-1 ${fullScreen ? 'mb-1 full-screen-button' : ''}`}>
-          {fullScreen ? 'Exit Full Screen' : 'Full Screen'}
+        <button
+          onClick={toggleFullScreen}
+          className={`mb-1 ${fullScreen ? "mb-1 full-screen-button" : ""}`}
+        >
+          {fullScreen ? "Exit Full Screen" : "Full Screen"}
         </button>
       </div>
       {!collapsed && (
-        <div className={`json-container ${fullScreen ? 'full-screen' : ''}`}>
+        <div className={`json-container ${fullScreen ? "full-screen" : ""}`}>
           <ReactJson src={JSON.parse(jsonData)} theme="shapeshifter" />
         </div>
       )}
@@ -44,14 +69,13 @@ const CollapsibleJSONViewer: FC <{ jsonData: string }> = ({ jsonData }) => {
   );
 };
 
-const Home: FC  = () => {
+const Home: FC = () => {
   const [jsonData, setJsonData] = useState<string>("");
   // const [searchTerm, setSearchTerm] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const jsonViewerRef = useRef<HTMLDivElement>(null);
 
- 
   const handleInputChange = (event: any) => {
     setJsonData(event.target.value);
     setError(null);
@@ -59,8 +83,8 @@ const Home: FC  = () => {
 
   const formatJson = (data: string) => {
     try {
-      if(jsonData == '') {
-        setError('Please Enter Something...')
+      if (jsonData == "") {
+        setError("Please Enter Something...");
       }
       const formattedJson = JSON.stringify(JSON.parse(data), null, 2);
       return formattedJson;
@@ -72,16 +96,19 @@ const Home: FC  = () => {
 
   const handlePaste = (event: ClipboardEvent) => {
     event.preventDefault();
-    const pastedText = event.clipboardData?.getData("text/plain") ?? '';
+    const pastedText = event.clipboardData?.getData("text/plain") ?? "";
     const formattedJson = formatJson(pastedText);
     setJsonData(formattedJson);
-    handleCopy();  
+    handleCopy();
     if (jsonViewerRef.current) {
       // jsonViewerRef.current.scrollIntoView({ behavior: 'smooth' });
       const offset = 10; // Adjust offset value as needed
-      const topOffset = jsonViewerRef.current.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({ top: topOffset, behavior: 'smooth' });
-    }    
+      const topOffset =
+        jsonViewerRef.current.getBoundingClientRect().top +
+        window.pageYOffset -
+        offset;
+      window.scrollTo({ top: topOffset, behavior: "smooth" });
+    }
   };
 
   const handleCopy = () => {
@@ -92,8 +119,6 @@ const Home: FC  = () => {
   };
 
   // const searchJson = () => {
-  
-
 
   // };
 
@@ -121,50 +146,61 @@ const Home: FC  = () => {
   // };
 
   const btnData = [
-    {'title':'Format JSON',  'event' : formatJson},
-    {'title':'Copy to Clipboard',  'event' : handleCopy},
-    {'title':'Search',  'event' : handleInputChange},
-    {'title':'Filter',  'event' : handleFilter},
-    {'title':'Navigate',  'event' : handleNavigation},
-    {'title':'Load from URL',  'event' : handleLoadURL},
+    { title: "Format JSON", event: formatJson },
+    { title: "Copy to Clipboard", event: handleCopy },
+    { title: "Search", event: handleInputChange },
+    { title: "Filter", event: handleFilter },
+    { title: "Navigate", event: handleNavigation },
+    { title: "Load from URL", event: handleLoadURL },
   ];
 
+  const { background, text } = theme.palette;
+
   return (
-    <div className='container'>
-      <h2>JSONIFY</h2>
-
-
-      <div className='main_section row'>
-        <div className='tool_box col-lg-6'>
-          <textarea ref={textAreaRef} value={jsonData} onInput={handleInputChange}
-            onPaste={handlePaste}  placeholder='Enter Json Data' style={{
-              width: "98%",
-              height: "400px",
-              padding: "10px",
-              fontSize: "16px",
-              border: "1px solid #ccc",
-              borderRadius: "5px",
-              resize: "none",
-            }}
-          ></textarea>
-          <div className="btn-box" >
-            {/* Button Rendering */}
-            {
-              btnData.map((items) => <Button title={items.title} onClick={items.event} /> )
-            }
-          </div>
-        </div>
-        <div className='response_box  col-lg-6'>
-            {error && <div style={{ color: "red" }}>{error}</div>}
-            <div ref={jsonViewerRef} className="json-box" >
-              <code >
-               {jsonData && <CollapsibleJSONViewer jsonData={jsonData} />}
-
-              </code>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h2">JSONIFY</Typography>
+      <Container maxWidth="100%" style={{ height: "100vh", display: "flex" }}>
+        <Box className="main_section row" style={{ flex: 1 }}>
+          <Box className="tool_box col-lg-6" style={{ width: "50%" }}>
+            <TextareaAutosize
+              ref={textAreaRef}
+              value={jsonData}
+              onInput={handleInputChange}
+              onPaste={handlePaste}
+              placeholder="Enter Json Data"
+              style={{
+                width: "98%",
+                height: "80vh",
+                padding: "10px",
+                fontSize: "16px",
+                border: `1px solid ${background.default}`,
+                borderRadius: "5px",
+                resize: "none",
+                backgroundColor: background.paper,
+                color: text.primary,
+              }}
+            />
+            <div className="btn-box">
+              {btnData.map((items) => (
+                <Button title={items.title} onClick={items.event} />
+              ))}
             </div>
-        </div>
-      </div>
-    </div>
+          </Box>
+          <Box className="tool_box col-lg-6" style={{ width: "50%" }}>
+            {error && <Typography style={{ color: "red" }}>{error}</Typography>}
+            <Box
+              ref={jsonViewerRef}
+              className="json-box"
+              style={{
+                minHeight: "80vh",
+              }}
+            >
+              {jsonData && <CollapsibleJSONViewer jsonData={jsonData} />}
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,9 @@
-import '../../assets/css/component.css'
-export default function button(props:BTN) {
-  return <button class="btn third" onClick={props.onClick} >{props.title}</button>
-}
+import Button from "@mui/material/Button";
 
+export default function button(props: BTN) {
+  return (
+    <Button variant="contained" onClick={props.onClick}>
+      {props.title}
+    </Button>
+  );
+}
